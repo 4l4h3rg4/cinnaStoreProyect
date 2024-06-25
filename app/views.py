12b8ae208c01656374262709524eadba5d25ffe7
Paula Molina <pau.molina@duocuc.cl>
2024-06-25 19:20:57 -0400

    Login view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 def index(request):
+    if request.method == 'POST':
+        print("post")
+        print(request.POST)
+        username = request.POST.get('username')
+        print('username:', username)
+        password = request.POST.get('password')
+        print('password:', password)
+        user = authenticate(request, username=username, password=password)
+        print('user:', user)
+    else:
+        print("get")
     return render(request, 'index.html')
 
 def catalog(request):
