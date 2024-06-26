0eab1753872153228347e3fe283023bced25b9b4
Paula Molina <pau.molina@duocuc.cl>
2024-06-25 20:48:49 -0400

    Remove txt-register

--- app/static/js/validaciones.js
+++ app/static/js/validaciones.js
@@ -1,12 +1,10 @@
 /* constantes del register */
 const register = document.getElementById('registro')
-const userRegister = document.getElementById('txt-register')
 const emailRegister = document.getElementById('email-register')
 const passwordRegister = document.getElementById('password-register')
 const terminos = document.getElementById('checkbox-terminosycondiciones')
 const emailBoxRegister = document.getElementById('email-box-register')
 const passwordBoxRegister = document.getElementById('password-box-register')
-const warningUserRegister = document.getElementById('warning-user-register')
 const warningEmailRegister = document.getElementById('warning-correo-register')
 const warningPasswordRegister = document.getElementById('warning-password-register')
 
@@ -57,30 +55,18 @@ login.addEventListener('submit', e => {
 /* validaciones de register */
 register.addEventListener('submit', e => {
     console.log('register submit')
-    if (userRegister.value.trim() === '' || emailRegister.value.trim() === '' || passwordRegister.value.trim() === '') {
-        warningUserRegister.textContent = 'Por favor, rellene todos los campos';
+    if (emailRegister.value.trim() === '' || passwordRegister.value.trim() === '') {
         warningEmailRegister.textContent = 'Por favor, rellene todos los campos';
         warningPasswordRegister.textContent = 'Por favor, rellene todos los campos';
-    } else if (userRegister.value.length < 6) {
-        warningUserRegister.textContent = 'El nombre de usuario debe tener al menos 6 caracteres';
     } else if (emailRegister.value.indexOf('@') === -1 || emailRegister.value.indexOf('.') === -1 || emailRegister.value.split('.')[1].trim() === '') {
         warningEmailRegister.textContent = 'Correo electrónico no válido';
     } else if (passwordRegister.value.length < 8) {
         warningPasswordRegister.textContent = 'La contraseña debe tener al menos 8 caracteres';
     } else {
-        warningUserRegister.textContent = '';
         warningEmailRegister.textContent = '';
         warningPasswordRegister.textContent = '';
-        // registrar usuario
-        console.log('Usuario registrado');
     }
 
-    /* cuando aparezca un mensaje de error, el css de lo que este abajo de dicho mensaje cambiara */
-    if (warningUserRegister.textContent !== '') {
-         emailBoxRegister.style.marginTop = '40px'
-    } else {
-         emailBoxRegister.style.marginTop = '0px'
-    }
     if (warningEmailRegister.textContent !== '') {
          passwordBoxRegister.style.marginTop = '20px';
     } else {
@@ -94,7 +80,6 @@ register.addEventListener('submit', e => {
       
     /* elimina los mensajes de error y vuelve al estado original los css luego de 3 segundos */
     setTimeout(() => {
-        warningUserRegister.textContent = '';
         warningEmailRegister.textContent = '';
         warningPasswordRegister.textContent = '';
         terminos.style.marginTop = '0px';
